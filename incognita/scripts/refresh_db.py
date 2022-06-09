74f9b85e0658f8ecd3d79cb892e6d0295cc3c6d2
momonala <mnalavadi@gmail.com>
2022-06-09 14:19:39 +0200

    refresh db script

---
+++ incognita/scripts/refresh_db.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from glob import glob
+
+from tqdm import tqdm
+
+from incognita.database import update_db
+
+logger = logging.getLogger("incognita.database")
+logger.setLevel(logging.WARNING)
+
+DB_FILE = "cache/geo_data.db"
+
+if os.path.exists(DB_FILE):
+    os.remove(DB_FILE)
+
+geo_files = glob("raw_data/*.geojson")
+for f in tqdm(geo_files):
+    update_db(f, DB_FILE)
