dae448393c626bb9dfbceafb2a403403a5e77817
momonala <mnalavadi@gmail.com>
2025-04-13 20:29:10 +0200

    add backup service for raw data

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,14 @@
 cp install/projects_overland_server.service  /lib/systemd/system/projects_overland_server.service
 cp install/projects_incognita_dashboard.service /lib/systemd/system/projects_incognita_dashboard.service
+cp install/projects_incognita_git_commit_scheduler.service /lib/systemd/system/projects_incognita_git_commit_scheduler.service
 
 sudo chmod 644 /lib/systemd/system/projects_overland_server.service
 sudo chmod 644 /lib/systemd/system/projects_incognita_dashboard.service
+sudo chmod 644 /lib/systemd/system/projects_incognita_git_commit_scheduler.service
 
 sudo systemctl daemon-reload
 sudo systemctl daemon-reexec
 
 sudo systemctl enable projects_overland_server.service
 sudo systemctl enable projects_incognita_dashboard.service
+sudo systemctl enable projects_incognita_git_commit_scheduler.service
