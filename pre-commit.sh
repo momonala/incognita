c3dc35dfe957a12c7f84cdea88c46165b89cf70f
momonala <mnalavadi@gmail.com>
2026-01-13 20:01:11 +0100

    improve config setup and install

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,14 +7,14 @@ echo "🧪 Running tests..."
 uv run pytest
 
 echo "🖤 Running black..."
-if ! uv run black . --check; then
+if ! uv run black incognita --check; then
     echo "❌ Black found formatting issues. To auto fix, run:"
     echo -e "\033[32muv run black .\033[0m"
     exit 1
 fi
 
 echo "🧼 Running ruff check..."
-if ! uv run ruff check .; then
+if ! uv run ruff check incognita; then
     echo "❌ Ruff found linting issues. To auto fix, run:"
     echo -e "\033[32muv run ruff check . --fix\033[0m"
     exit 1
